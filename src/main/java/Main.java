e390ba6bff928a471ec567c76e1a062890adb8f9
Mokhtar SAMY <60510677+MokhtarSamy@users.noreply.github.com>
2022-05-24 03:50:44 +0200

    Graph update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,94 @@
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.OVertex;
 import graph.Person;
 
 public class Main {
 
-    private ODatabaseDocument db;
-    public void newGraph(){
-        OVertex person = db.newInstance(Person.class.getName());
-        person.save();
-    }
     public static void main(String[] args) {
+        OrientDB orientDB = new OrientDB("remote:localhost/", OrientDBConfig.defaultConfig());
+
+        // Replace the arguments with your own database name and user/password
+        // A remplacer avec le nom de la base de donnée et les identifiants
+        ODatabaseSession db = orientDB.open("testdb", "root", "2610");
+
+        if (db.getClass("Product") == null) {
+            OClass product = db.createVertexClass("Product");
+            product.createProperty("asin", OType.STRING);
+            product.createProperty("title", OType.STRING);
+            product.createProperty("price", OType.FLOAT);
+            product.createProperty("imgUrl", OType.STRING);
+            product.createIndex("product_asin_index", OClass.INDEX_TYPE.UNIQUE, "asin");
+        }
+
+        //Partie modèle Graph: pour mettre tous dans un seul main
+        if (db.getClass("Person") == null) {
+            OClass person = db.createVertexClass("Person");
+            person.createProperty("id", OType.INTEGER);
+            person.createProperty("firstName", OType.STRING);
+            person.createProperty("lastName", OType.FLOAT);
+            person.createProperty("gender", OType.STRING);
+            person.createProperty("birthday", OType.DATE);
+            person.createProperty("creationDate", OType.DATE);
+            person.createProperty("locationIP", OType.STRING);
+            person.createProperty("browserUsed", OType.STRING);
+            person.createProperty("place", OType.INTEGER);
+            person.createIndex("Person_id_index", OClass.INDEX_TYPE.UNIQUE, "id");
+        }
+
+        if (db.getClass("Post") == null) {
+            OClass post = db.createVertexClass("Post");
+            post.createProperty("id", OType.INTEGER);
+            post.createProperty("imageFile", OType.BINARY);
+            post.createProperty("creationDate", OType.DATE);
+            post.createProperty("locationIP", OType.STRING);
+            post.createProperty("browserUsed", OType.STRING);
+            post.createProperty("language", OType.STRING);
+            post.createProperty("content", OType.STRING);
+            post.createProperty("length", OType.INTEGER);
+            post.createIndex("Post_id_index", OClass.INDEX_TYPE.UNIQUE, "id");
+        }
+
+        if (db.getClass("Tag") == null) {
+            OClass tag = db.createVertexClass("Tag");
+            tag.createProperty("id", OType.INTEGER);
+            tag.createProperty("name", OType.STRING);
+            tag.createIndex("Tag_id_index", OClass.INDEX_TYPE.UNIQUE, "id");
+        }
+
+        if (db.getClass("Knows") == null) {
+            db.createEdgeClass("Knows");
+        }
+        if (db.getClass("HasTag") == null) {
+            db.createEdgeClass("HasTag");
+        }
+        if (db.getClass("HasInterest") == null) {
+            db.createEdgeClass("HasInterest");
+        }
+        if (db.getClass("HasCreated") == null) {
+            db.createEdgeClass("HasCreated");
+        }
+
+        /* Exemple pour ajouter des records
+
+        OVertex v1 = db.newVertex("Tag");
+        v1.setProperty("name", "OneRF");
+        v1.save();
+
+        OVertex v2 = db.newVertex("Post");
+        v2.setProperty("content", "TwoTwosSS");
+        v2.save();
+
+        v1.addEdge(v2, "HasTag").save();
+
+        db.commit();
+        */
+        db.close();
+        orientDB.close();
 
     }
 }
