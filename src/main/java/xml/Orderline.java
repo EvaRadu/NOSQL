3911eee2bacb0ed6409dd083abb486e4b121608e
Mia Swery <mia.swery@etu.univ-cotedazur.fr>
2022-06-13 14:23:46 +0200

    retrieval of invoices

---
+++ src/main/java/xml/Orderline.java
@@ -0,0 +1,5 @@
+package xml;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
