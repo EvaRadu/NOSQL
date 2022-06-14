b9b7871114fc9b0a0d0b253cdb92f364b4a0a78d
Hugo Bulzomi <hugo.bulzomi@etu.unice.fr>
2022-06-14 17:36:21 +0200

    Implémentation de query 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,29 @@
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OVertex;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
+import feedback.FeedbackLoader;
 import json.JsonsLoader;
+import org.xml.sax.SAXException;
 import relational.CustomerLoader;
 import relational.VendorLoader;
 import xml.InvoiceLoader;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseException, IOException, org.json.simple.parser.ParseException {
+    public static void main(String[] args) throws ParseException, ParserConfigurationException, IOException, SAXException, org.json.simple.parser.ParseException {
         OrientDB orientDB = new OrientDB("remote:localhost/", OrientDBConfig.defaultConfig());
 
         // Replace the arguments with your own database name and user/password
@@ -22,15 +31,28 @@ public class Main {
         ODatabaseSession db = orientDB.open("testdb", "root", "2610");
 
         // LOADING THE PRODUCT DATA
-        //VendorLoader vendorLoader = new VendorLoader(db);
-        //vendorLoader.load();
-        //JsonsLoader jsonLoader = new JsonsLoader(db);
-        //jsonLoader.load();
-        //jsonLoader.createOutEdges();
+        VendorLoader vendorLoader = new VendorLoader(db);
         /*
+        VendorLoader vendorLoader = new VendorLoader(db);
+        vendorLoader.load();
+        JsonsLoader jsonLoader = new JsonsLoader(db);
+        jsonLoader.load();
+
         InvoiceLoader invoiceLoader = new InvoiceLoader(db);
         invoiceLoader.load();
-        */
+        jsonLoader.createOutEdges();
+        System.out.println("EDGE OK");
+        FeedbackLoader feedbackLoader = new FeedbackLoader(db);
+        feedbackLoader.chargementFeedback(); */
+        System.out.println("OK");
+        ArrayList<String> test = query3(db, "2020-03-10", "2020-07-10", "B003D9RBMU");
+        for(String t:test){
+            System.out.println(t);
+        }
+        CustomerLoader customerLoader = new CustomerLoader(db);
+        customerLoader.load();
+
+
 
 
 
@@ -55,13 +77,13 @@ public class Main {
         // LOADING THE PRODUCT DATA
         JsonLoader jsonLoader = new JsonLoader(db);
         jsonLoader.load();
-*/
         // LOADING THE CUSTOMER DATA
         CustomerLoader customerLoader = new CustomerLoader(db);
         customerLoader.load();
         // LOADING THE VENDOR DATA
         VendorLoader vendorLoader = new VendorLoader(db);
         vendorLoader.load();
+*/
         // exemple de création d'un document, qui sera dans GENERIC CLASS dans la BD
         // pour voir les données dans une classe, choisi la classe et après fait QUERY ALL
 
@@ -82,6 +104,7 @@ public class Main {
         /* -------------------------- */
         /* --- TESTS 4.4 CUSTOMERS -- */
         /* -------------------------- */
+
 
         ODocument docCustomer1 = new ODocument("Customer");
         docCustomer1.field( "id", "123" );
@@ -120,7 +143,6 @@ public class Main {
         docsCustomer.add(docCustomer1);
         docsCustomer.add(docCustomer3);
 
-
         customerLoader.insertOneCustomer(db,docCustomer1);
         customerLoader.updateOneCustomer(db,docCustomer2);
         customerLoader.deleteOneCustomer(db,docCustomer2);
@@ -155,6 +177,7 @@ public class Main {
         docsVendor.add(docVendor2);
         docsVendor.add(docVendor3);
 
+        vendorLoader = new VendorLoader(db);
 
         vendorLoader.insertOneVendor(db,docVendor1);
         vendorLoader.updateOneVendor(db,docVendor2);
@@ -165,5 +188,49 @@ public class Main {
         vendorLoader.deleteManyVendors(db,docsVendor);
 
 
+    }
+
+    public static ArrayList<String> query3(ODatabaseSession db, String from, String to, String product_asin){
+        // On retourne tous les commentaires des Feedbacks sur le produit asin avec une note en dessous de la moyenne,
+        // et le contenu des Posts posté entre les dates from et to.
+        ArrayList<String> res = new ArrayList<>();
+        String query = "SELECT * from Feedback where productAsin = ?";
+        OResultSet rs = db.query(query, product_asin);
+
+        while(rs.hasNext()){
+            Optional<OEdge> optional = rs.next().getEdge();
+            if (optional.isPresent()) {
+                OEdge feedback = optional.get();
+                String text = (String)feedback.getProperty("comment");
+                String grade = "";
+                int cpt = 1;
+                while(text.charAt(cpt) !=",".charAt(0)){
+                    grade = grade + text.charAt(cpt);
+                    cpt ++;
+                }
+                float sentiment = Float.parseFloat(grade);
+
+                if(sentiment<2.5){
+                    res.add(text);
+                }
+            }
+        }
+        rs.close();
+
+
+        query = "SELECT * from Post where creationDate between ? and ?";
+        OResultSet rs2 = db.query(query, from, to);
+
+        while(rs2.hasNext()){
+            Optional<OVertex> optional = rs2.next().getVertex();
+            if (optional.isPresent()) {
+                OVertex post = optional.get();
+                String text = (String)post.getProperty("content");
+                res.add(text);
+            }
+        }
+
+
+        return res;
     }
 }
