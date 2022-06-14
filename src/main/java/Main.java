9aef8e0b1d4573e7bb110659620cbad083edef0a
DESKTOP-UAD63F6\Eva <evaradu@hotmail.fr>
2022-06-14 10:18:54 +0200

    about to merge

---
+++ src/main/java/Main.java
@@ -0,0 +1,87 @@
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import json.JsonsLoader;
+import org.xml.sax.SAXException;
+import relational.VendorLoader;
+import xml.InvoiceLoader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException, IOException, org.json.simple.parser.ParseException, ParserConfigurationException, SAXException {
+        OrientDB orientDB = new OrientDB("remote:localhost/", OrientDBConfig.defaultConfig());
+
+        // Replace the arguments with your own database name and user/password
+        // A remplacer avec le nom de la base de donnée et les identifiants
+        ODatabaseSession db = orientDB.open("testdb", "root", "2610");
+
+        // LOADING THE PRODUCT DATA
+        VendorLoader vendorLoader = new VendorLoader(db);
+        vendorLoader.load();
+        JsonsLoader jsonLoader = new JsonsLoader(db);
+        jsonLoader.load();
+        jsonLoader.createOutEdges();
+
+        InvoiceLoader invoiceLoader = new InvoiceLoader(db);
+        invoiceLoader.load();
+
+
+
+
+
+
+        //FeedbackLoader.chargementFeedback(db);
+
+        //GraphLoader.createSocialNetworkGraph(db);
+        /* Exemple pour ajouter des records
+
+        OVertex v1 = db.newVertex("Tag");
+        v1.setProperty("name", "OneRF");
+        v1.save();
+
+        OVertex v2 = db.newVertex("Post");
+        v2.setProperty("content", "TwoTwosSS");
+        v2.save();
+
+        v1.addEdge(v2, "HasTag").save();
+
+        db.commit();
+        */
+
+
+/*
+        // LOADING THE PRODUCT DATA
+        JsonLoader jsonLoader = new JsonLoader(db);
+        jsonLoader.load();
+
+        // LOADING THE CUSTOMER DATA
+        CustomerLoader customerLoader = new CustomerLoader(db);
+        customerLoader.load();
+
+
+        // LOADING THE VENDOR DATA
+        VendorLoader vendorLoader = new VendorLoader(db);
+        vendorLoader.load();
+
+*/
+        // exemple de création d'un document, qui sera dans GENERIC CLASS dans la BD
+        // pour voir les données dans une classe, choisi la classe et après fait QUERY ALL
+
+        /*
+        ODocument doc = new ODocument("Persons");
+        doc.field( "name", "Luke" );
+        doc.field( "surname", "Skywalker" );
+        doc.field( "city", "lalala");
+
+        // SAVE THE DOCUMENT
+        db.save(doc);
+        db.commit();
+
+        db.close();
+    */
+    }
+}
